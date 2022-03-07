352efee01bfc99e2f80e96ef03edf2bf99b7d1bd
SHIVKANT AGNIHOTRI <shivagni01@gmail.com>
2022-03-07 04:43:49 +0000

    second commit

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 // Your First Program
 
-class HelloWorld THIS IS NEOSOFT {
+class HelloWorld THIS IS NEOSOFT FROM MUMBAI {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
